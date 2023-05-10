ad8ceed5cce7454855ea40b9b39d97440b416a8d
Nada-NF <nadanfaidh6@gmail.com>
2023-05-10 19:35:08 +0200

    done

--- frontend/announcement/src/pages/authentication.js
+++ frontend/announcement/src/pages/authentication.js
@@ -1,7 +1,40 @@
 export const Authentication =()=>{
     return(
-        <div>
-           
+        <div className="auth">
+           <Login />
+           <Register />
+        </div>
+    )
+}
+
+const Login=()=>{
+    return(
+        <div className="login">
+            <h1>Login</h1>
+            <form>
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+                <button>Login</button>
+            </form>
+        </div>
+    )
+}
+const Register=()=>{
+    return(
+        <div className="register">
+            
+            <form>
+            <h1>Register</h1>
+                <div>
+                    <label htmlFor="username">User Name</label>
+                <input type="text" id="username" placeholder="Username" onChange={(event)=>} />
+                </div>
+                <div>
+                    <label htmlFor="password">User Name</label>
+                <input type="text" id="password" placeholder="Username" onChange={(event)=>} />
+                </div>
+            </form>
+            
         </div>
     )
 }
